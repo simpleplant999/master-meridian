25ff008693ce4046b2f5c933d986433a21e844aa
Ced <59819169+simpleplant999@users.noreply.github.com>
2021-09-11 02:52:52 +0800

    fixed dropdown and collapse

---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,307 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+    
+    <!-- Custom fonts for this template-->
+    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+    <link
+    href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+    rel="stylesheet">
+    
+    <!-- Custom styles for this template-->
+    <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+    
+    <link rel="stylesheet" href="assets/css/style.css">
+    
+    <!-- Styles -->
+    {{-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous"> --}}
+    {{-- <link href="{{ asset('css/app.css') }}" rel="stylesheet"> --}}
+    
+    <title>Document</title>
+</head>
+<body id="page-top">
+    <div id="app">
+        <!-- Page Wrapper -->
+        <div id="wrapper">
+            
+            <!-- Sidebar -->
+            {{-- <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion d-none d-sm-block d-md-none toggled" id="accordionSidebar"> --}}
+                <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="myDIV">
+                    
+                    <!-- Sidebar - Brand -->
+                    <a class="sidebar-brand d-flex align-items-center justify-content-center" href="index.html">
+                        <img src="assets/img/logo.svg" class="img-fluid d-none d-md-block" alt="">
+                        <img src="assets/img/logo2.png" class="img-fluid w-50 d-block d-md-none" alt="">
+                    </a>
+                    
+                    <!-- Divider -->
+                    <hr class="sidebar-divider my-0">
+                    
+                    <!-- Nav Item - Dashboard -->
+                    
+                    <li class="nav-item">
+                        <a to="/" class="nav-link" href="/">
+                            <i class="fas fa-fw fa-tachometer-alt"></i>
+                            <span>DASHBOARD</span>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a to="/teammates" class="nav-link" href="teammates.html">
+                            <i class="fas fa-fw fa-users"></i>
+                            <span>TEAMMATES</span>
+                        </a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a to="/zoom" class="nav-link" href="zoom.html">
+                            <i class="fas fa-fw fa-video"></i>
+                            <span>ZOOM LINKS</span>
+                        </a>
+                    </li>
+                    <!-- <li class="nav-item">
+                        <a class="nav-link" href="index.html">
+                            <i class="fas fa-fw fa-user-clock"></i>
+                            <span>EOD</span>
+                        </a>
+                    </li> -->
+                    
+                    <li class="nav-item">
+                        <a to="referral" class="nav-link" href="referral.html">
+                            <i class="fas fa-fw fa-user-plus"></i>
+                            <span>REFERRAL</span>
+                        </a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a to="expenses-liquidation" class="nav-link" href="index.html">
+                            <i class="fas fa-fw fa-dollar-sign"></i>
+                            <span>EXPENSES LIQUIDATION</span>
+                        </a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a to="schedules" class="nav-link" href="index.html">
+                            <i class="fas fa-fw fa-calendar-alt"></i>
+                            <span>SCHEDULES</span>
+                        </a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a to="erica-todo" class="nav-link" href="index.html">
+                            <i class="fas fa-fw fa-list-ul"></i>
+                            <span>ERICA TODO</span>
+                        </a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a to="logins" class="nav-link" href="index.html">
+                            <i class="fas fa-fw fa-sign-in-alt"></i>
+                            <span>LOGINS</span>
+                        </a>
+                    </li>   
+                    
+                    <li class="nav-item">
+                        <a to="/master-leads" class="nav-link" href="index.html">
+                            <i class="fas fa-fw fa-project-diagram"></i>
+                            <span>MASTER LEADS</span>
+                        </a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a to="/sales-commission" class="nav-link" href="index.html">
+                            <i class="fas fa-fw fa-dollar-sign"></i>
+                            <span>SALES COMMISSION</span>
+                        </a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link collapsed" data-toggle="collapse" href="#collapseExample" role="button" aria-expanded="false" aria-controls="collapseExample">
+                            <i class="fas fa-fw fa-money-bill-alt"></i>
+                            <span>EXPENSES</span>
+                        </a>
+                        <div id="collapseExample" class="collapse" aria-labelledby="collapseExample">
+                            <div class="bg-white py-2 collapse-inner rounded">
+                                <h6 class="collapse-header">YEARLY EXPENSES:</h6>
+                                <a class="collapse-item" href="#">2020</a>
+                                <a class="collapse-item" href="#">2021</a>
+                            </div>
+                        </div>
+                    </li>
+                    {{-- <li class="nav-item">
+                        <a to="/expenses" class="nav-link" href="index.html">
+                            <i class="fas fa-fw fa-money-bill-alt"></i>
+                            <span>EXPENSES</span>
+                        </a>
+                    </li> --}}
+                    
+                </ul>
+                <!-- End of Sidebar -->
+                
+                <!-- Content Wrapper -->
+                <div id="content-wrapper" class="d-flex flex-column">
+                    
+                    <!-- Main Content -->
+                    <div id="content">
+                        
+                        <!-- Topbar -->
+                        <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+                            
+                            <!-- Sidebar Toggle (Topbar) -->
+                            <button onclick="myFunction()" class="btn btn-link d-md-none rounded-circle mr-3">
+                                <i class="fa fa-bars text-primary2"></i>
+                            </button>
+                            
+                            <!-- Topbar Search -->
+                            <!-- <form
+                                class="d-none d-sm-inline-block form-inline mr-auto ml-md-3 my-2 my-md-0 mw-100 navbar-search">
+                                <div class="input-group">
+                                    <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..."
+                                    aria-label="Search" aria-describedby="basic-addon2">
+                                    <div class="input-group-append">
+                                        <button class="btn btn-primary" type="button">
+                                            <i class="fas fa-search fa-sm"></i>
+                                        </button>
+                                    </div>
+                                </div>
+                            </form> -->
+                            
+                            <!-- Topbar Navbar -->
+                            <ul class="navbar-nav ml-auto">
+                                
+                                <!-- Nav Item - Search Dropdown (Visible Only XS) -->
+                                <li class="nav-item dropdown no-arrow d-sm-none">
+                                    <a class="nav-link dropdown-toggle" href="#" id="searchDropdown" role="button"data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="fas fa-search fa-fw"></i>
+                                    </a>
+                                    <!-- Dropdown - Messages -->
+                                    <div class="dropdown-menu dropdown-menu-right p-3 shadow animated--grow-in" aria-labelledby="searchDropdown">
+                                        <form class="form-inline mr-auto w-100 navbar-search">
+                                            <div class="input-group">
+                                                <input type="text" class="form-control bg-light border-0 small" placeholder="Search for..." aria-label="Search" aria-describedby="basic-addon2">
+                                                <div class="input-group-append">
+                                                    <button class="btn btn-primary" type="button">
+                                                        <i class="fas fa-search fa-sm"></i>
+                                                    </button>
+                                                </div>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </li>
+                                
+                                <!-- Nav Item - User Information -->
+                                <li class="nav-item dropdown no-arrow">
+                                    <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <span class="mr-2 d-none d-lg-inline text-gray-600 small">Marion Balandra</span>
+                                        <img class="img-profile rounded-circle"
+                                        src="assets/img/undraw_profile.svg">
+                                    </a>
+                                    <!-- Dropdown - User Information -->
+                                    <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
+                                        <a class="dropdown-item" href="#">
+                                            <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                            Profile
+                                        </a>
+                                        <a class="dropdown-item" href="#">
+                                            <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
+                                            Settings
+                                        </a>
+                                        <a class="dropdown-item" href="#">
+                                            <i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
+                                            Activity Log
+                                        </a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
+                                            <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
+                                            Logout
+                                        </a>
+                                    </div>
+                                </li>
+                                
+                            </ul>
+                            
+                        </nav>
+                        <!-- End of Topbar -->
+                        
+                        <!-- Begin Page Content -->
+                        <div class="container-fluid">
+                            
+                            <!-- Page Heading -->
+                            {{-- <router-view/> --}}
+                            <main class="py-4">
+                                @yield('content')
+                            </main>
+                            
+                        </div>
+                        
+                        
+                    </div>
+                    <!-- End of Main Content -->
+                    
+                    <!-- Footer -->
+                    <footer class="sticky-footer bg-white">
+                        <div class="container my-auto">
+                            <div class="copyright text-center my-auto">
+                                <span>Copyright &copy; Meridian Remote Teams 2020</span>
+                            </div>
+                        </div>
+                    </footer>
+                    <!-- End of Footer -->
+                    
+                </div>
+                <!-- End of Content Wrapper -->
+                
+            </div>
+            <!-- End of Page Wrapper -->
+            
+            <!-- Scroll to Top Button-->
+            <a class="scroll-to-top rounded" href="#page-top">
+                <i class="fas fa-angle-up"></i>
+            </a>
+            
+            <!-- Logout Modal-->
+            <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
+                            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                                <span aria-hidden="true">×</span>
+                            </button>
+                        </div>
+                        <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
+                        <div class="modal-footer">
+                            <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
+                            <a class="btn btn-primary" href="login.html">Logout</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Bootstrap core JavaScript-->
+        <script src="assets/vendor/jquery/jquery.min.js"></script>
+        
+        {{-- bs-5 --}}
+        {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script> --}}
+        {{-- bs-4 --}}
+        {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-Piv4xVNRyMGpqkS2by6br4gNJ7DXjqk09RmUpJ8jgGtD7zP9yug3goQfGII0yAns" crossorigin="anonymous"></script> --}}
+        {{-- <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script> --}}
+        {{-- <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script> --}}
+        
+        <!-- Core plugin JavaScript-->
+        <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+        
+        <!-- Custom scripts for all pages-->
+        <script src="assets/js/sb-admin-2.min.js"></script>
+    </body>
+    </html>
